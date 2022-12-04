0713307f4e357a9605ff8bb86f2b7cda3c0bf81b
CodyAdam <cody.ad4@gmail.com>
2022-12-04 06:13:54 +0100

    feat: day4

---
+++ python/4.2.py
@@ -0,0 +1,37 @@
+import sys
+
+try:
+    with open("input.txt") as file:
+        file = file.read()
+except:
+    with sys.stdin as file:
+        file = file.read()
+
+lines = [line.strip() for line in file.splitlines()]
+# subList = [theList[n:n+N] for n in range(0, len(theList), N)]
+
+s= 0
+for line in lines:
+    a, b = line.split(",")
+    a = list(map(int, a.split("-")))
+    b = list(map(int, b.split("-")))
+
+
+    a0, a1 = a
+    b0, b1 = b
+
+    valid = False
+    if b0 >= a0 and b1 <= a1:
+        s += 1
+        valid = True
+    elif a0 >= b0 and a1 <= b1:
+        valid = True
+        s += 1
+    elif b0 <= a1 and b1 >= a0:
+        s += 1
+        valid = True
+    elif a0 <= b1 and a1 >= b0:
+        valid = True
+        s += 1
+    print(a, b, "yes" if valid else "")
+print(s)
--- readme.md
+++ readme.md
@@ -6,4 +6,6 @@ Advent of Code is an Advent calendar of small programming puzzles for a variety 
 | day | time solved | difficulty |
 | --- | ----------- | ---------- |
 | 1   | 9 min       | 1/5        |
-| 1   | 20 min      | 2/5        |
+| 2   | 20 min      | 2/5        |
+| 3   | 15 min      | 2/5        |
+| 4   | 13 min      | 2/5        |
