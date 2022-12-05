b83331bdc98ea4ce39e234728bee7fd3e37cda0e
CodyAdam <cody.ad4@gmail.com>
2022-12-05 06:41:04 +0100

    feat: clean the code

--- python/5.1.py
+++ python/5.1.py
@@ -3,33 +3,32 @@ from collections import defaultdict
 with open("input.txt") as file:
     file = file.read()
 
+# PARSE INPUT
 board, commands = file.split("\n\n")
 board = board.replace("[", " ").replace("]", " ").splitlines()[:-1]
 commands = [line.strip() for line in commands.splitlines()]
 stack = defaultdict(lambda: [])
 for index in range(len(board) - 1, 0 - 1, -1):
     line = board[index][1:] + " "
-    print("---- ", index, "'" + line + "'")
 
     for i in range(0, len(line), 4):
-        print(i // 4, line[i])
         if line[i] == " ": continue
         else: stack[i // 4].append(line[i])
 
 print(stack)
 
+# EXECUTE COMMANDS
 for command in commands:
-    count, from_, to = command.replace("move ",
-                                       "").replace(" from ",
-                                                   " ").replace(" to ",
-                                                                " ").split()
-    count, from_, to = int(count), int(from_) - 1, int(to) - 1
-    print(count, from_, to)
+    command = command.replace("move ", "")
+    command = command.replace(" from ", " ")
+    command = command.replace(" to ", " ")
+    count, from_, to = map(int, command.split())
+    to_add = []
     for i in range(count):
-        stack[to].append(stack[from_].pop())
-
+        to_add.append(stack[from_ - 1].pop())
+    for x in to_add:
+        stack[to - 1].append(x)
 
 for i in range(len(stack)):
     print(stack[i][-1], end="")
-
 print()
