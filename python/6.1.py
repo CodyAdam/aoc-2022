ef79366a39e6d3323bbe144cb07adce67e46d171
CodyAdam <cody.ad4@gmail.com>
2022-12-06 06:29:55 +0100

    feat: simplify

--- python/6.1.py
+++ python/6.1.py
@@ -3,15 +3,10 @@ from collections import defaultdict
 with open("input.txt") as file:
     file = file.read()
 
-lines = file.splitlines()
-for line in lines:
-
-    done = False
-    for i in range(len(line) - 14):
-        if done:
-            break
-        if len(line[i:i + 14]) == len(set(line[i:i + 14])):
-            done = True
+window = 14
+for line in file.splitlines():
+    for i in range(len(line) - window):
+        if window == len(set(line[i:i + window])):
             break
 
-    print(line[i:i + 14], i+14)
+    print(line[i:i + window], i + window)
